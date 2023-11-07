726f30098d40f248f08357733c556656d0508838
RelexSun <88622289+RelexSun@users.noreply.github.com>
2023-11-07 23:21:04 +0700

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,25 @@
 // we are not using "type": "modules" so instead of importing we use require.
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 
+// express app
 const app = express();
 
+// const dotenv = require("dotenv");
+// dotenv.config(); this is also acceptable
+
+// connect to the database
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// listen for requests
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
